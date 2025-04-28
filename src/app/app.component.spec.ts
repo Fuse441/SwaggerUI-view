c54b71c0b840730ced39c358d27cf3e8d03e8f62
Fuse441 <jiranon.khemklad@gmail.com>
2025-04-28 18:30:31 +0700

    Change : change title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'SwaggerUI-test' title`, () => {
+  it(`should have the 'SwaggerUI' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('SwaggerUI-test');
+    expect(app.title).toEqual('SwaggerUI');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, SwaggerUI-test');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, SwaggerUI');
   });
 });
